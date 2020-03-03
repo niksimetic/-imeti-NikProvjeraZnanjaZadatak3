11092485a8b8e36bc0ba96250d6ca0fdf629f72a
niksimetic <55134764+niksimetic@users.noreply.github.com>
2020-03-03 13:14:41 +0100

    Add files via upload

---
+++ ŠimetićNikProvjeraZnanjaZadatak3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ŠimetićNikProvjeraZnanjaZadatak3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+
+            Console.WriteLine("Upisite pozitivni broj: ");
+            Console.ReadLine();
+            a = Convert.ToInt32();
+            if (a < 0)
+            {
+                Console.WriteLine("Broj neodgovara");
+            }
+            else
+            {
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
